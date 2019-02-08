12c599da52abf102e1162621a06a3aa5141cecdd
Matthew James <matthew.james@digital.justice.gov.uk>
2019-02-08 09:33:06 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get '/about'  =>  'static_pages#about'
   get '/contact'  =>  'static_pages#contact'
   get '/signup' =>  'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
